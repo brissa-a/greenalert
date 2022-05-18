15f3ccd3a69e245afd4a5eb09e052a2238ce7781
alxthm <git@alxthm.com>
2022-05-18 21:12:42 +0200

    add a proper home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/img/spongebob.jpeg" className="App-logo" alt="Mocking spongebob meme" />
-        <p>
-          fEliX PrÉfèrE rEaCT.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <h1>Header</h1>
+            </header>
+
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/our-team">Our team</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <main>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<div>Home Page</div>}/>
+                        <Route path="*" element={<div>404 not found</div>}/>
+                    </Routes>
+                </BrowserRouter>
+            </main>
+            <footer>
+                <p>©Copyright 2050 by nobody. All rights reversed.</p>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
