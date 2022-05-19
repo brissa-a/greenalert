0c922a913174b25f25306204df70e93843138b37
fe51 <veith.felix@gmail.com>
2022-05-19 10:42:02 +0200

    feat: Added link to main v0 pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
+import ReportAnAdd from './pages/report-add';
+import AboutGreenwashing from './pages/about-greenwashing';
+
 function App() {
     return (
         <div className="App">
@@ -12,6 +15,8 @@ function App() {
             <nav>
                 <ul>
                     <li><a href="/">Home</a></li>
+                    <li><a href="/report-an-add">Signaler une pub</a></li>
+                    <li><a href="/about-greenwahsing">Comprendre le Greenwhasing</a></li>
                     <li><a href="/our-team">Our team</a></li>
                     <li><a href="#">Projects</a></li>
                     <li><a href="#">Contact</a></li>
@@ -22,6 +27,8 @@ function App() {
                     <Routes>
                         <Route path="/" element={<div>Home Page</div>}/>
                         <Route path="*" element={<div>404 not found</div>}/>
+                        <Route path="/report-an-add"element={<ReportAnAdd/>} />
+                        <Route path="/about-greenwahsing"element={<AboutGreenwashing/>} />
                     </Routes>
                 </BrowserRouter>
             </main>
