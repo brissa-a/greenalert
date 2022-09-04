97cd27c789eab47eb7c5b741e91b7f6a4deb4bbb
DESKTOP-6GR70BD\alexi <alexis.brissard@gmail.com>
2022-09-04 16:52:04 +0200

    Use react-router link instead of a

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 import './App.css';
 import ReportAnAd from './pages/report-ad';
@@ -9,34 +9,35 @@ import { HomePage } from './pages/HomePage';
 
 function App() {
     return (
-        <div className="App flex flex-col">
-            <header className="flex w-full bg-ga-green text-green-50 justify-center items-center">
-                <h1 className="text-3xl">Green Alert</h1>
-            </header>
-            <nav>
-                <ul className='flex flex-row justify-evenly'>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/report-an-ad">Signaler une pub</a></li>
-                    <li><a href="/about-greenwahsing">Comprendre le Greenwhasing</a></li>
-                    <li><a href="/our-team">Our team</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
-            <main className="flex flex-col items-center justify-center">
-                <BrowserRouter>
+        <BrowserRouter>
+            <div className="App flex flex-col">
+                <header className="flex w-full bg-ga-green text-green-50 justify-center items-center">
+                    <h1 className="text-3xl">Green Alert</h1>
+                </header>
+                <nav>
+                    <ul className='flex flex-row justify-evenly'>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/report-an-ad">Signaler une pub</Link></li>
+                        <li><Link to="/about-greenwahsing">Comprendre le Greenwhasing</Link></li>
+                        <li><Link to="/our-team">Our team</Link></li>
+                        <li><Link to="#">Projects</Link></li>
+                        <li><Link to="#">Contact</Link></li>
+                    </ul>
+                </nav>
+                <main className="flex flex-col items-center justify-center">
+
                     <Routes>
-                        <Route path="/" element={<HomePage/>} />
+                        <Route path="/" element={<HomePage />} />
                         <Route path="*" element={<div>404 not found</div>} />
                         <Route path="/report-an-ad" element={<ReportAnAd />} />
                         <Route path="/about-greenwahsing" element={<AboutGreenwashing />} />
                     </Routes>
-                </BrowserRouter>
-            </main>
-            <footer>
-                <p>©Copyright 2050 by nobody. All rights reversed.</p>
-            </footer>
-        </div>
+                </main>
+                <footer>
+                    <p>©Copyright 2050 by nobody. All rights reversed.</p>
+                </footer>
+            </div>
+        </BrowserRouter>
     );
 }
 
